19ab8825b485cb4b63e49e4680ca9bbafcd20ce6
Norbert Orzechowicz <norbert@orzechowicz.pl>
2014-05-04 19:42:37 +0200

    Added getError method to Matcher

--- src/Coduo/PHPMatcher/Matcher.php
+++ src/Coduo/PHPMatcher/Matcher.php
@@ -10,13 +10,29 @@ class Matcher
      */
     private $matcher;
 
+    /**
+     * @param PropertyMatcher $matcher
+     */
     public function __construct(PropertyMatcher $matcher)
     {
         $this->matcher = $matcher;
     }
 
+    /**
+     * @param mixed $value
+     * @param mixed $pattern
+     * @return bool
+     */
     public function match($value, $pattern)
     {
         return $this->matcher->match($value, $pattern);
+    }
+
+    /**
+     * @return null|string
+     */
+    public function getError()
+    {
+        return $this->matcher->getError();
     }
 }
