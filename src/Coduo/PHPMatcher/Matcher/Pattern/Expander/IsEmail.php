6140007cd684c29850d1cdf98e594cabb4f66962
Piotr Jura <piotr.jura@schibsted.pl>
2014-11-03 16:55:42 +0100

    IsEmail expander

---
+++ src/Coduo/PHPMatcher/Matcher/Pattern/Expander/IsEmail.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Coduo\PHPMatcher\Matcher\Pattern\Expander;
+
+use Coduo\PHPMatcher\Matcher\Pattern\PatternExpander;
+use Coduo\ToString\String;
+
+class IsEmail implements PatternExpander
+{
+    /**
+     * @var null|string
+     */
+    private $error;
+
+    /**
+     * @param string $value
+     * @return boolean
+     */
+    public function match($value)
+    {
+        if (false === is_string($value)) {
+            $this->error = sprintf("IsEmail expander require \"string\", got \"%s\".", new String($value));
+            return false;
+        }
+
+        if (false === $this->matchValue($value)) {
+            $this->error = sprintf("string \"%s\" is not a valid e-mail address.", $value);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param string $value
+     * @return bool
+     */
+    protected function matchValue($value)
+    {
+        try {
+            return false !== filter_var($value, FILTER_VALIDATE_EMAIL);
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+}
