92658a2334e08597db8dde436e0642a6d78edbe4
Piotr Jura <piotr.jura@schibsted.pl>
2014-11-03 15:35:05 +0100

    DateTime pattern expander without tests yet

---
+++ src/Coduo/PHPMatcher/Matcher/Pattern/Expander/IsDateTime.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Coduo\PHPMatcher\Matcher\Pattern\Expander;
+
+use Coduo\PHPMatcher\Matcher\Pattern\PatternExpander;
+use Coduo\ToString\String;
+
+class IsDateTime implements PatternExpander
+{
+    /**
+     * @var null|string
+     */
+    private $error;
+
+    /**
+     * @param string $value
+     * @return boolean
+     */
+    public function match($value)
+    {
+        if (!is_string($value)) {
+            $this->error = sprintf("IsDateTime expander require \"string\", got \"%s\".", new String($value));
+            return false;
+        }
+
+        if ($this->matchValue($value)) {
+            $this->error = sprintf("string \"%s\" is not a valid date.", $value);
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @return string|null
+     */
+    public function getError()
+    {
+        return $this->error;
+    }
+
+    /**
+     * @param string $value
+     * @return bool
+     */
+    protected function matchValue($value)
+    {
+        try {
+            new \DateTime($value);
+            return true;
+        } catch (\Exception $e) {
+            return false;
+        }
+    }
+}
