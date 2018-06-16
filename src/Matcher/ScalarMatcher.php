1850bafa97eaf6897b1938d0b2e313d54fd358a0
Mateusz Sip <mateusz.sip@gmail.com>
2018-06-16 18:02:52 +0200

    ScalarMatcher suppoorts CaseInsensitive modifier

--- src/Matcher/ScalarMatcher.php
+++ src/Matcher/ScalarMatcher.php
@@ -4,14 +4,38 @@ declare(strict_types=1);
 
 namespace Coduo\PHPMatcher\Matcher;
 
+use Coduo\PHPMatcher\Matcher\Modifier\CaseInsensitive;
+use Coduo\PHPMatcher\Matcher\Modifier\MatcherModifier;
 use Coduo\ToString\StringConverter;
 
-final class ScalarMatcher extends Matcher
+final class ScalarMatcher extends ModifiableMatcher
 {
+    const SUPPORTED_MODIFIERS = [
+        CaseInsensitive::NAME
+    ];
+
+    /** @var bool */
+    private $caseInsensitive;
+
+    public function __construct()
+    {
+        $this->caseInsensitive = false;
+    }
+
     public function match($value, $pattern) : bool
     {
-        if ($value !== $pattern) {
-            $this->error = \sprintf('"%s" does not match "%s".', new StringConverter($value), new StringConverter($pattern));
+        if (!$this->caseInsensitive && $value !== $pattern) {
+            $this->setError('does not match', $value, $pattern);
+            return false;
+        }
+
+        if (
+            $this->caseInsensitive
+            && \is_string($value)
+            && \is_string($pattern)
+            && \strcasecmp($value, $pattern) !== 0
+        ) {
+            $this->setError('does not case-insensitive match', $value, $pattern);
             return false;
         }
 
@@ -21,5 +45,32 @@ final class ScalarMatcher extends Matcher
     public function canMatch($pattern) : bool
     {
         return \is_scalar($pattern);
+    }
+
+    public function supportedModifiers(): array
+    {
+        return self::SUPPORTED_MODIFIERS;
+    }
+
+    public function getMatchers(): array
+    {
+        return [];
+    }
+
+    public function applyModifier(MatcherModifier $modifier)
+    {
+        switch ($modifier->getName()) {
+            case CaseInsensitive::NAME:
+                $this->caseInsensitive= true;
+                break;
+        }
+    }
+
+    /**
+     * @return void
+     */
+    private function setError(string $message, $value, $pattern)
+    {
+        $this->error = \sprintf('"%s" ' . $message . ' "%s".', new StringConverter($value), new StringConverter($pattern));
     }
 }
--- tests/Matcher/ScalarMatcherTest.php
+++ tests/Matcher/ScalarMatcherTest.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Coduo\PHPMatcher\Tests\Matcher;
 
+use Coduo\PHPMatcher\Matcher\Modifier\CaseInsensitive;
 use Coduo\PHPMatcher\Matcher\ScalarMatcher;
 use PHPUnit\Framework\TestCase;
 
@@ -55,6 +56,34 @@ class ScalarMatcherTest extends TestCase
         $this->assertEquals($error, $matcher->getError());
     }
 
+    /**
+     * @dataProvider positiveCaseInsensitiveMatches
+     */
+    public function test_positive_case_insensitive_matches($value, $pattern)
+    {
+        $matcher = $this->caseInsensitiveMatcher();
+        $this->assertTrue($matcher->match($value, $pattern));
+    }
+
+    /**
+     * @dataProvider negativeCaseInsensitiveMatches
+     */
+    public function test_negative_case_insensitive_matches($value, $pattern)
+    {
+        $matcher = $this->caseInsensitiveMatcher();
+        $this->assertFalse($matcher->match($value, $pattern));
+    }
+
+    /**
+     * @dataProvider negativeCaseInsensitiveMatchDescription
+     */
+    public function test_negative_case_insensitive_match_description($value, $pattern, $error)
+    {
+        $matcher = $this->caseInsensitiveMatcher();
+        $matcher->match($value, $pattern);
+        $this->assertEquals($error, $matcher->getError());
+    }
+
     public static function negativeMatches()
     {
         return [
@@ -102,5 +131,41 @@ class ScalarMatcherTest extends TestCase
             [1.1, false, '"1.1" does not match "false".'],
             [false, ['foo', 'bar'], '"false" does not match "Array(2)".'],
         ];
+    }
+
+    public static function positiveCaseInsensitiveMatches()
+    {
+        return [
+            ['michal', 'Michal'],
+            ['Michal', 'michal'],
+            ['fooBar', 'FOObAR'],
+            ['Bazinga', 'baZIngA']
+        ];
+    }
+
+    public static function negativeCaseInsensitiveMatches()
+    {
+        return [
+            ['michal', 'Michall'],
+            ['fooBar', 'barFoo'],
+            ['Bazinga', 'BaIng']
+        ];
+    }
+
+    public static function negativeCaseInsensitiveMatchDescription()
+    {
+        return [
+            ['michal', 'Michall', '"michal" does not case-insensitive match "Michall".'],
+            ['fooBar', 'barFoo', '"fooBar" does not case-insensitive match "barFoo".'],
+            ['Bazinga', 'BaIng', '"Bazinga" does not case-insensitive match "BaIng".']
+        ];
+    }
+
+    private function caseInsensitiveMatcher(): ScalarMatcher
+    {
+        $matcher = new ScalarMatcher();
+        $matcher->applyModifier(new CaseInsensitive());
+
+        return $matcher;
     }
 }
